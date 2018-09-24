b4852e25842e9b0405b0114ec8662ad203f18fb7
Maykonn <maykonn@outlook.com>
2018-09-24 18:36:20 -0300

    New optional param to timestamp based random numbers

--- UniqueNumber.js
+++ UniqueNumber.js
@@ -1,18 +1,19 @@
 class UniqueNumber {
 
   /**
-   * Generates a random number with a fixed length (optional).
-   * If a fixed length is not provided something random between 1 and the length of
-   * Number.MAX_SAFE_INTEGER will be used as length, which is the default behavior.
+   * Generates a random number with a fixed length (optional) and/or timestamp based (optional).
    *
-   * @param {number} length Optional fixed length
+   * @param {number} length (optional) if not given will be something between 1 and the length of Number.MAX_SAFE_INTEGER
+   * @param {boolean} timestampBased (optional) if the number will be randomized by a timestamp or not
    */
-  constructor(length = 0) {
+  constructor(length = 0, timestampBased = false) {
     if (length && typeof length !== 'number') {
       throw new Error('The length param must be a number');
     }
 
     this._value = undefined;
+    this._timestampBased = Boolean(timestampBased);
+
     this._length = this._calculateLength(length);
     this._value = this._generate();
   }
@@ -34,10 +35,16 @@ class UniqueNumber {
   }
 
   _generate() {
-    return Math.floor(
-      Math.pow(10, this._length - 1) +
-      Math.random() * (Math.pow(10, this._length) - Math.pow(10, this._length - 1) - 1)
-    );
+    let number = 0;
+
+    if (!this._timestampBased) {
+      number = Math.floor(
+        Math.pow(10, this._length - 1) +
+        Math.random() * (Math.pow(10, this._length) - Math.pow(10, this._length - 1) - 1)
+      );
+    }
+
+    return number;
   }
 
 }
