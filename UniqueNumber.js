665d3e6ecc6f105c8dff2fb35a4c1f63d4ea5362
Maykonn <maykonn@outlook.com>
2018-09-24 18:07:47 -0300

    Improved generation of random numbers Now is possible to configure a fixed length or not

--- UniqueNumber.js
+++ UniqueNumber.js
@@ -1,23 +1,44 @@
 class UniqueNumber {
 
-    /**
-     * @param {number} length
-     */
-    constructor(length) {
-        this._length = length;
-        this._value = this._generate();
+  /**
+   * Generates a random number with a fixed length (optional).
+   * If a fixed length is not provided something random between 1 and the length of
+   * Number.MAX_SAFE_INTEGER will be used as length, which is the default behavior.
+   *
+   * @param {number} length Optional fixed length
+   */
+  constructor(length = 0) {
+    if (length && typeof length !== 'number') {
+      throw new Error('The length param must be a number');
     }
 
-    getValue() {
-        return this._value;
+    this._value = undefined;
+    this._length = this._calculateLength(length);
+    this._value = this._generate();
+  }
+
+  getValue() {
+    return this._value;
+  }
+
+  _calculateLength(length) {
+    const maxLength = Number.MAX_SAFE_INTEGER.toString().length;
+
+    if (length > 0 && length > maxLength) {
+      length = maxLength;
+    } else if (!length) {
+      length = Math.floor(Math.random() * maxLength) + 1;
     }
 
-    _generate() {
-        return Math.floor(
-            Math.pow(10, this._length - 1) +
-            Math.random() * (Math.pow(10, this._length) - Math.pow(10, this._length - 1) - 1)
-        );
-    }
+    return length;
+  }
+
+  _generate() {
+    return Math.floor(
+      Math.pow(10, this._length - 1) +
+      Math.random() * (Math.pow(10, this._length) - Math.pow(10, this._length - 1) - 1)
+    );
+  }
 
 }
 
