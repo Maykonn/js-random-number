92ab44c398ee25fe51e8bf7aaed42c24ade9b6e7
Maykonn <maykonn@outlook.com>
2018-09-25 18:20:25 -0300

    Generator refactored

--- UniqueNumberGenerator.js
+++ UniqueNumberGenerator.js
@@ -1,30 +1,38 @@
-const UniqueNumber = require('./UniqueNumber.js');
-const UniqueNumberTimestampBased = require('./UniqueNumberTimestampBased.js');
+const NumberLength = require('./NumberLength.js');
+const RandomNumber = require('./RandomNumber.js');
+const RandomNumberTimestampBased = require('./RandomNumberTimestampBased.js');
 
 class UniqueNumberGenerator {
 
-  static timestampBased() {
-    this._timestampBased = true;
-    return this;
+  /**
+   * Generates a random number with a fixed length (optional) and/or timestamp based (optional).
+   *
+   * @param {number} length (optional) if not given will be something between 1 and the length of Number.MAX_SAFE_INTEGER
+   * @param {boolean} timestampBased (optional) if the number will be randomized by a timestamp or not
+   */
+  constructor(length = 0, timestampBased = false) {
+    if (length && typeof length !== 'number') {
+      throw new Error('The length param must be a number');
+    }
+
+    this._value = this._generate(length, timestampBased);
   }
 
   /**
-   * Use the length param to define the number length
-   * @param {number} length
-   * @returns {UniqueNumber}
+   * @return {RandomNumberTimestampBased|RandomNumber}
    */
-  static getRandomNumber(length = 0) {
-    return new UniqueNumber(length, this._timestampBased);
+  getValue() {
+    return this._value;
   }
 
-  /**
-   * Generates a random unique number as string based on time
-   * @param {number} length If you need to keep more than one number with same length, set the pad param to be > 0
-   * @return {UniqueNumberTimestampBased}
-   */
-  // static getRandomNumberTimestampBased(length = 0) {
-  //   return new UniqueNumberTimestampBased(length);
-  // }
+  _generate(length, timestampBased) {
+    return (
+      Boolean(timestampBased) ?
+        new RandomNumberTimestampBased(new NumberLength(length)) :
+        new RandomNumber(new NumberLength(length))
+    );
+  }
+
 }
 
 module.exports = UniqueNumberGenerator;
