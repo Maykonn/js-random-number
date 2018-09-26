22f9f38e8b49161c177c8e8676bf6ab5a5e80e72
Maykonn <maykonn@outlook.com>
2018-09-26 17:36:37 -0300

    Configuration docs

--- Configuration.js
+++ Configuration.js
@@ -4,6 +4,9 @@ class Configuration {
     this.reset();
   }
 
+  /**
+   * @return {Configuration}
+   */
   reset() {
     this._minLength = undefined;
     this._maxLength = undefined;
@@ -11,6 +14,10 @@ class Configuration {
     return this;
   }
 
+  /**
+   * @param {number} length
+   * @return {Configuration}
+   */
   setLength(length) {
     this._validateLength(length);
     this._minLength = length;
@@ -18,35 +25,60 @@ class Configuration {
     return this;
   }
 
+  /**
+   * @param {number} length
+   * @return {Configuration}
+   */
   setMinLength(length) {
     this._validateLength(length);
     this._minLength = length;
     return this;
   }
 
+  /**
+   * @return {number}
+   */
   getMinLength() {
     return this._minLength;
   }
 
+  /**
+   * @param {number} length
+   * @return {Configuration}
+   */
   setMaxLength(length) {
     this._validateLength(length);
     this._maxLength = length;
     return this;
   }
 
+  /**
+   * @return {number}
+   */
   getMaxLength() {
     return this._maxLength;
   }
 
+  /**
+   * @return {Configuration}
+   */
   timestampBased() {
     this._timestampBased = true;
     return this;
   }
 
+  /**
+   * @return {boolean}
+   */
   isTimestampBased() {
     return Boolean(this._timestampBased);
   }
 
+  /**
+   * @param {number} length
+   * @return {boolean}
+   * @private
+   */
   _validateLength(length) {
     if (length && typeof length !== 'number') {
       throw new Error('The length param must be a number');
