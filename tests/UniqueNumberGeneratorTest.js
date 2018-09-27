a5fa7e4bc5ee712ab267de4f5cd5ab993b439b4d
Maykonn <maykonn@outlook.com>
2018-09-27 12:29:30 -0300

    Length tests

--- tests/UniqueNumberGeneratorTest.js
+++ tests/UniqueNumberGeneratorTest.js
@@ -1,9 +1,20 @@
 "use strict";
 
+const Configuration = require('../Configuration.js');
 const UniqueNumberGenerator = require('../UniqueNumberGenerator.js');
-const UniqueNumber = new UniqueNumberGenerator();
- console.log('Unique Number:', UniqueNumber.getValue());
 
-const UniqueNumberTimestampBased = new UniqueNumberGenerator(0, true);
-console.log('Unique Number:', UniqueNumberTimestampBased.getValue());
-console.log('Unique Number Timestamp:', UniqueNumberTimestampBased.getValue().getTimestamp());
+const NumberConfig = new Configuration();
+NumberConfig.setMinLength(1);
+NumberConfig.setMaxLength(16);
+
+const UniqueNumber = new UniqueNumberGenerator(NumberConfig);
+console.log('Unique Number:', UniqueNumber.getValue());
+
+// NumberConfig.reset();
+// NumberConfig.timestampBased();
+// const UniqueNumberTimestampBased = new UniqueNumberGenerator(NumberConfig);
+// console.log('Unique Number:', UniqueNumberTimestampBased.getValue());
+// console.log('Unique Number Timestamp:', UniqueNumberTimestampBased.getValue().getTimestamp());
+
+
+
