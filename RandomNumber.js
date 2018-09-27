7433f278a169d52b39eab947c6a07da49e048752
Maykonn <maykonn@outlook.com>
2018-09-27 14:08:14 -0300

    Refactored

--- RandomNumber.js
+++ RandomNumber.js
@@ -1,9 +1,11 @@
 const AbstractRandomNumber = require('./AbstractRandomNumber.js');
 const Configuration = require('./Configuration.js');
+const NumberLength = require('./NumberLength.js');
 
 class RandomNumber extends AbstractRandomNumber {
 
   /**
+   * A random number
    * @param {Configuration} NumberConfiguration
    */
   constructor(NumberConfiguration) {
@@ -19,31 +21,38 @@ class RandomNumber extends AbstractRandomNumber {
      * @type {number}
      * @private
      */
-    this._length = this._value.toString().length;
+    this._length = new NumberLength(this._value.toString().length);
   }
 
   /**
+   * Generates a random number
    * @return {number}
    * @protected
    */
   _generate() {
-    const minLength = this.getConfiguration().getMinLength().getValue();
-    const maxLength = this.getConfiguration().getMaxLength().getValue();
+    const length = this._calculateLength();
 
-    // randomize a length between min and max length configured
-    const length = Math.floor(
-      minLength +
-      (Math.random() * (maxLength + 1 - minLength))
-    );
-
-    // generate a random number
     const number = Math.floor(
       Math.pow(10, length - 1) +
       (Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1))
     );
 
-    // increasing the number randomness
     return super._increaseNumberRandomness(number);
+  }
+
+  /**
+   * Randomize a length between configured min and max length
+   * @return {number}
+   * @private
+   */
+  _calculateLength() {
+    const minLength = this.getConfiguration().getMinLength().getValue();
+    const maxLength = this.getConfiguration().getMaxLength().getValue();
+
+    return Math.floor(
+      minLength +
+      (Math.random() * (maxLength + 1 - minLength))
+    );
   }
 
 }
