6b4c5b891ccfc4b2c903b121543119776a968eaa
Maykonn <maykonn@outlook.com>
2018-09-28 11:23:07 -0300

    Fixed examples

--- tests/UniqueNumberGeneratorTest.js
+++ tests/UniqueNumberGeneratorTest.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const Configuration = require('../src/Configuration.js');
-const UniqueNumberGenerator = require('../RandomNumberGenerator.js');
+const RandomNumberGenerator = require('../RandomNumberGenerator.js');
 
 // Configuring the random number generation
 const NumberConfig = new Configuration();
@@ -9,9 +9,9 @@ NumberConfig.setMinLength(8);
 NumberConfig.setMaxLength(16);
 
 // Generates a random number
-const UniqueNumber = new UniqueNumberGenerator(NumberConfig);
-console.log('Unique Number:');
-console.log(UniqueNumber.getValue());
+const RandomNumber = new RandomNumberGenerator(NumberConfig);
+console.log('Random Number:');
+console.log(RandomNumber.getValue());
 console.log('\n');
 
 // Reconfiguring the random number generation
@@ -19,6 +19,6 @@ NumberConfig.reset();
 NumberConfig.timestampBased();
 
 // Generates a random number timestamp based
-const UniqueNumberTimestampBased = new UniqueNumberGenerator(NumberConfig);
-console.log('Unique Number timestamp based:');
-console.log(UniqueNumberTimestampBased.getValue());
+const RandomNumberTimestampBased = new RandomNumberGenerator(NumberConfig);
+console.log('Random Number timestamp based:');
+console.log(RandomNumberTimestampBased.getValue());
