ac51c7c03190703d803ed7ff8617630175b6b7f4
Maykonn <maykonn@outlook.com>
2018-09-28 10:08:50 -0300

    Tests fixed

--- tests/UniqueNumberGeneratorTest.js
+++ tests/UniqueNumberGeneratorTest.js
@@ -3,17 +3,22 @@
 const Configuration = require('../Configuration.js');
 const UniqueNumberGenerator = require('../UniqueNumberGenerator.js');
 
+// Configuring the random number generation
 const NumberConfig = new Configuration();
+NumberConfig.setMinLength(8);
+NumberConfig.setMaxLength(16);
 
+// Generates a random number
+const UniqueNumber = new UniqueNumberGenerator(NumberConfig);
+console.log('Unique Number:');
+console.log(UniqueNumber.getValue());
+console.log('\n');
 
-// const UniqueNumber = new UniqueNumberGenerator(NumberConfig);
-// console.log('Unique Number:', UniqueNumber.getValue());
-
-// NumberConfig.reset();
+// Reconfiguring the random number generation
+NumberConfig.reset();
 NumberConfig.timestampBased();
+
+// Generates a random number timestamp based
 const UniqueNumberTimestampBased = new UniqueNumberGenerator(NumberConfig);
-console.log('Unique Number:', UniqueNumberTimestampBased.getValue());
-//console.log('Unique Number Timestamp:', UniqueNumberTimestampBased.getValue().getTimestamp());
-
-
-
+console.log('Unique Number timestamp based:');
+console.log(UniqueNumberTimestampBased.getValue());
